9d22e0a258a3d57f369162a2b08d3ec6b6e73b75
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-13 19:03:00 +0200

    Create the driver model class

---
+++ app/src/main/java/com/lince23/f1info/features/drivers/domain/Driver.kt
@@ -0,0 +1,9 @@
+package com.lince23.f1info.features.drivers.domain
+
+data class Driver(
+    val fullName: String,
+    val number: String,
+    val country: String,
+    val image: String,
+    val team: String
+)
