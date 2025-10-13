6254dd83f74de12f9f513fd8e239ce0d24e2f767
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-13 17:16:08 +0200

    Initial commit

---
+++ app/src/main/java/com/lince23/f1info/features/drivers/presentation/DriversListActivity.kt
@@ -0,0 +1,21 @@
+package com.lince23.f1info.features.drivers.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lince23.f1info.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_drivers_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
