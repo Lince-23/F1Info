97931c546d489d60674ed7a2bcb1e430d5104842
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-13 17:20:57 +0200

    Initial commit

--- app/src/main/java/com/lince23/f1info/features/drivers/presentation/DriversListActivity.kt
+++ app/src/main/java/com/lince23/f1info/features/drivers/presentation/DriversListActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.lince23.f1info.R
 
-class MainActivity : AppCompatActivity() {
+class DriversListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
