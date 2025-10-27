bbc37e035bf5751d61e5e6188cd4c96cd65878e2
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-27 01:24:52 +0100

    Changed base_url

--- app/src/main/java/com/lince23/f1info/core/api/ApiClient.kt
+++ app/src/main/java/com/lince23/f1info/core/api/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class ApiClient {
-    private val BASE_URL = "http://api.jolpi.ca/ergast/f1/"
+    private val BASE_URL = "https://api.jolpi.ca/ergast/f1/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
